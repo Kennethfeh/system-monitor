baa0cd4331a53dc453036c817e02b891a84f6ee6
kenneth feh <kennethfeh@kenneths-MacBook-Pro.local>
2023-03-16 14:30:00 +0900

    Add enhanced architecture with modular design
    - Refactor into internal packages (collector, models, storage)
    - Add comprehensive metrics collection with more details
    - Implement metrics history storage for charts
    - Add temperature sensor support
    - Improve network statistics with error/drop tracking
    - Add graceful shutdown handling
    - Add command-line flags for configuration
    - Enhanced UI with more system information display

--- main.go
+++ main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
+	"context"
 	"embed"
 	"encoding/json"
+	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
-	"github.com/shirou/gopsutil/v3/cpu"
-	"github.com/shirou/gopsutil/v3/disk"
-	"github.com/shirou/gopsutil/v3/host"
-	"github.com/shirou/gopsutil/v3/mem"
-	"github.com/shirou/gopsutil/v3/net"
+	"github.com/kennethfeh/system-monitor/internal/collector"
+	"github.com/kennethfeh/system-monitor/internal/models"
+	"github.com/kennethfeh/system-monitor/internal/storage"
 )
 
 //go:embed static/*
 var staticFiles embed.FS
+
+//go:embed templates/*
+var templateFiles embed.FS
+
+var (
+	port     = flag.String("port", "8080", "Port to run the server on")
+	interval = flag.Duration("interval", 2*time.Second, "Metrics collection interval")
+	history  = flag.Int("history", 60, "Number of historical data points to keep")
+)
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
@@ -25,182 +39,210 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-type SystemMetrics struct {
-	Timestamp   int64            `json:"timestamp"`
-	CPU         CPUMetrics       `json:"cpu"`
-	Memory      MemoryMetrics    `json:"memory"`
-	Disk        []DiskMetrics    `json:"disk"`
-	Network     []NetworkMetrics `json:"network"`
-	System      SystemInfo       `json:"system"`
+type Server struct {
+	collector *collector.Collector
+	storage   *storage.MetricsStorage
+	clients   map[*websocket.Conn]bool
+	broadcast chan models.SystemMetrics
+	register  chan *websocket.Conn
+	unregister chan *websocket.Conn
 }
 
-type CPUMetrics struct {
-	UsagePercent []float64 `json:"usage_percent"`
-	TotalPercent float64   `json:"total_percent"`
-	Cores        int       `json:"cores"`
+func NewServer(collector *collector.Collector, storage *storage.MetricsStorage) *Server {
+	return &Server{
+		collector:  collector,
+		storage:    storage,
+		clients:    make(map[*websocket.Conn]bool),
+		broadcast:  make(chan models.SystemMetrics),
+		register:   make(chan *websocket.Conn),
+		unregister: make(chan *websocket.Conn),
+	}
 }
 
-type MemoryMetrics struct {
-	Total       uint64  `json:"total"`
-	Used        uint64  `json:"used"`
-	Free        uint64  `json:"free"`
-	UsedPercent float64 `json:"used_percent"`
-}
-
-type DiskMetrics struct {
-	Device      string  `json:"device"`
-	Mountpoint  string  `json:"mountpoint"`
-	Total       uint64  `json:"total"`
-	Used        uint64  `json:"used"`
-	Free        uint64  `json:"free"`
-	UsedPercent float64 `json:"used_percent"`
-}
-
-type NetworkMetrics struct {
-	Name        string `json:"name"`
-	BytesSent   uint64 `json:"bytes_sent"`
-	BytesRecv   uint64 `json:"bytes_recv"`
-	PacketsSent uint64 `json:"packets_sent"`
-	PacketsRecv uint64 `json:"packets_recv"`
-}
-
-type SystemInfo struct {
-	Hostname string `json:"hostname"`
-	OS       string `json:"os"`
-	Platform string `json:"platform"`
-	Uptime   uint64 `json:"uptime"`
-}
-
-func collectMetrics() (*SystemMetrics, error) {
-	metrics := &SystemMetrics{
-		Timestamp: time.Now().Unix(),
-	}
-
-	// CPU metrics
-	cpuPercent, err := cpu.Percent(time.Second, true)
-	if err == nil {
-		metrics.CPU.UsagePercent = cpuPercent
-		total := 0.0
-		for _, p := range cpuPercent {
-			total += p
-		}
-		if len(cpuPercent) > 0 {
-			metrics.CPU.TotalPercent = total / float64(len(cpuPercent))
-		}
-	}
-	
-	cpuCount, err := cpu.Counts(true)
-	if err == nil {
-		metrics.CPU.Cores = cpuCount
-	}
-
-	// Memory metrics
-	vmStat, err := mem.VirtualMemory()
-	if err == nil {
-		metrics.Memory = MemoryMetrics{
-			Total:       vmStat.Total,
-			Used:        vmStat.Used,
-			Free:        vmStat.Free,
-			UsedPercent: vmStat.UsedPercent,
-		}
-	}
-
-	// Disk metrics
-	partitions, err := disk.Partitions(false)
-	if err == nil {
-		for _, partition := range partitions {
-			usage, err := disk.Usage(partition.Mountpoint)
-			if err == nil && usage.Total > 0 {
-				metrics.Disk = append(metrics.Disk, DiskMetrics{
-					Device:      partition.Device,
-					Mountpoint:  partition.Mountpoint,
-					Total:       usage.Total,
-					Used:        usage.Used,
-					Free:        usage.Free,
-					UsedPercent: usage.UsedPercent,
-				})
-			}
-		}
-	}
-
-	// Network metrics
-	netIO, err := net.IOCounters(true)
-	if err == nil {
-		for _, io := range netIO {
-			if io.BytesSent > 0 || io.BytesRecv > 0 {
-				metrics.Network = append(metrics.Network, NetworkMetrics{
-					Name:        io.Name,
-					BytesSent:   io.BytesSent,
-					BytesRecv:   io.BytesRecv,
-					PacketsSent: io.PacketsSent,
-					PacketsRecv: io.PacketsRecv,
-				})
-			}
-		}
-	}
-
-	// System info
-	hostInfo, err := host.Info()
-	if err == nil {
-		metrics.System = SystemInfo{
-			Hostname: hostInfo.Hostname,
-			OS:       hostInfo.OS,
-			Platform: hostInfo.Platform,
-			Uptime:   hostInfo.Uptime,
-		}
-	}
-
-	return metrics, nil
-}
-
-func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Printf("WebSocket upgrade failed: %v", err)
-		return
-	}
-	defer conn.Close()
-
-	ticker := time.NewTicker(2 * time.Second)
-	defer ticker.Stop()
-
+func (s *Server) run() {
 	for {
 		select {
-		case <-ticker.C:
-			metrics, err := collectMetrics()
-			if err != nil {
-				log.Printf("Failed to collect metrics: %v", err)
-				continue
+		case client := <-s.register:
+			s.clients[client] = true
+			log.Println("Client connected")
+			
+			// Send historical data to new client
+			history := s.storage.GetHistory()
+			for _, metrics := range history {
+				if err := client.WriteJSON(metrics); err != nil {
+					log.Printf("Error sending historical data: %v", err)
+					break
+				}
 			}
 
-			if err := conn.WriteJSON(metrics); err != nil {
-				log.Printf("Failed to write metrics: %v", err)
-				return
+		case client := <-s.unregister:
+			if _, ok := s.clients[client]; ok {
+				delete(s.clients, client)
+				client.Close()
+				log.Println("Client disconnected")
+			}
+
+		case metrics := <-s.broadcast:
+			for client := range s.clients {
+				err := client.WriteJSON(metrics)
+				if err != nil {
+					log.Printf("Error writing to client: %v", err)
+					client.Close()
+					delete(s.clients, client)
+				}
 			}
 		}
 	}
 }
 
-func handleAPI(w http.ResponseWriter, r *http.Request) {
-	metrics, err := collectMetrics()
+func (s *Server) handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("WebSocket upgrade error: %v", err)
+		return
+	}
+	
+	s.register <- conn
+	
+	defer func() {
+		s.unregister <- conn
+	}()
+	
+	// Keep connection alive and handle ping/pong
+	for {
+		_, _, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+	}
+}
+
+func (s *Server) handleIndex(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFS(templateFiles, "templates/index.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	
+	data := struct {
+		Title string
+		Port  string
+	}{
+		Title: "System Monitor",
+		Port:  *port,
+	}
+	
+	if err := tmpl.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
+func (s *Server) handleAPIMetrics(w http.ResponseWriter, r *http.Request) {
+	metrics, err := s.collector.Collect()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(metrics)
 }
 
-func main() {
-	// Serve static files
-	http.Handle("/", http.FileServer(http.FS(staticFiles)))
+func (s *Server) handleAPIHistory(w http.ResponseWriter, r *http.Request) {
+	history := s.storage.GetHistory()
 	
-	// API endpoints
-	http.HandleFunc("/api/metrics", handleAPI)
-	http.HandleFunc("/ws", handleWebSocket)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(history)
+}
 
-	port := ":8080"
-	fmt.Printf("System Monitor started on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+func (s *Server) startMetricsCollection(ctx context.Context) {
+	ticker := time.NewTicker(*interval)
+	defer ticker.Stop()
+	
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			metrics, err := s.collector.Collect()
+			if err != nil {
+				log.Printf("Error collecting metrics: %v", err)
+				continue
+			}
+			
+			s.storage.Add(metrics)
+			s.broadcast <- metrics
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	
+	log.Printf("Starting System Monitor on port %s", *port)
+	log.Printf("Collection interval: %v", *interval)
+	log.Printf("History size: %d data points", *history)
+	
+	// Initialize components
+	collector := collector.NewCollector()
+	storage := storage.NewMetricsStorage(*history)
+	server := NewServer(collector, storage)
+	
+	// Start WebSocket handler
+	go server.run()
+	
+	// Start metrics collection
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go server.startMetricsCollection(ctx)
+	
+	// Setup routes
+	router := mux.NewRouter()
+	
+	// API routes
+	router.HandleFunc("/api/metrics", server.handleAPIMetrics).Methods("GET")
+	router.HandleFunc("/api/history", server.handleAPIHistory).Methods("GET")
+	router.HandleFunc("/ws", server.handleWebSocket)
+	
+	// Static files
+	router.PathPrefix("/static/").Handler(http.FileServer(http.FS(staticFiles)))
+	
+	// Main page
+	router.HandleFunc("/", server.handleIndex).Methods("GET")
+	
+	// Setup HTTP server
+	srv := &http.Server{
+		Addr:         ":" + *port,
+		Handler:      router,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+	
+	// Graceful shutdown
+	go func() {
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+		<-sigChan
+		
+		log.Println("Shutting down server...")
+		cancel()
+		
+		shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer shutdownCancel()
+		
+		if err := srv.Shutdown(shutdownCtx); err != nil {
+			log.Printf("Server shutdown error: %v", err)
+		}
+	}()
+	
+	// Start server
+	fmt.Printf("\nSystem Monitor is running at http://localhost:%s\n", *port)
+	fmt.Println("Press Ctrl+C to stop")
+	
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Server error: %v", err)
+	}
+	
+	log.Println("Server stopped")
 }
